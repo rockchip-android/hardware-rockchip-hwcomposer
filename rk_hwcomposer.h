1c16b2b30038f0705ef8fdc8096291a8a4b14539
huangds <hds@rock-chips.com>
2015-03-19 09:32:40 +0800

    modify hwc version 2.01

--- rk_hwcomposer.h
+++ rk_hwcomposer.h
@@ -67,7 +67,7 @@
 ( \
     ((n) + ((align) - 1)) & ~((align) - 1) \
 )
-#define GHWC_VERSION  "2.00"
+#define GHWC_VERSION  "2.01"
 //HWC version Tag
 //Get commit info:  git log --format="Author: %an%nTime:%cd%nCommit:%h%n%n%s%n%n"
 //Get version: busybox strings /system/lib/hw/hwcomposer.rk30board.so | busybox grep HWC_VERSION
