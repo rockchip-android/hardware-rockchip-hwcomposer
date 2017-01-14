961c1c6fdda0b0a25de66a761d3fff400f47be95
ZhuangXiaoLiang <zhuangxl@rock-chips.com>
2017-01-14 20:15:54 +0800

    Fix mem leak when using DrmFramebuffer.
    Change-Id: Ie63a609450172707cfa963a12e1dacb1338ad035

--- glworker.cpp
+++ glworker.cpp
@@ -719,7 +719,8 @@ void GLWorkerCompositor::Finish() {
   glFinish();
 
   char use_framebuffer_cache_opt[PROPERTY_VALUE_MAX];
-  property_get("hwc.drm.use_framebuffer_cache", use_framebuffer_cache_opt, "1");
+  //disable use_framebuffer_cache,otherwise it will lead mem leak when re-allocate DrmFramebuffer
+  property_get("hwc.drm.use_framebuffer_cache", use_framebuffer_cache_opt, "0");
   bool use_framebuffer_cache = atoi(use_framebuffer_cache_opt);
 
   if (use_framebuffer_cache) {
