79c5f19429217c3f31f5f908d878151686b182cb
ZhuangXiaoLiang <zhuangxl@rock-chips.com>
2017-07-12 16:45:37 +0800

    ==========update to version 0.29=========
    Change-Id: I4b102c48683a13e567bec8371491b5b059ff5775
    Signed-off-by: ZhuangXiaoLiang <zhuangxl@rock-chips.com>

--- drmhwcomposer.h
+++ drmhwcomposer.h
@@ -28,7 +28,7 @@
 #include "hwc_debug.h"
 
 /*hwc version*/
-#define GHWC_VERSION                    "0.28"
+#define GHWC_VERSION                    "0.29"
 
 /* hdr usage */
 #define HDRUSAGE                                       0x3000000
